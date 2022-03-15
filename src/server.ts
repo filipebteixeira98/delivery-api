ac35adfcfbf29a3155af46ee3f1186d58c9e2b5a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-15 08:01:49 -0300

    chore: added error handling

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import { routes } from './routes';
 
@@ -7,6 +8,19 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+);
 
 app.listen(3000, () =>
   console.log('🚀 Server is running at http://localhost:3000')
