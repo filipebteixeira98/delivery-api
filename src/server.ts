b60fa41dd8ef580532b31360190cd25e31da6ad3
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-15 01:28:15 -0300

    chore: added route to create client

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
+import { routes } from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: true });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () =>
   console.log('🚀 Server is running at http://localhost:3000')
