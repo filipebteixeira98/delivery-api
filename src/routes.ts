5745c14ed53921c310e33ef4141ba7565ab6b51f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-16 19:40:49 -0300

    chore: added route to create deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -4,14 +4,20 @@ import { AuthenticateClientController } from './modules/account/authenticateClie
 
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
+
 const routes = Router();
 
 const createClientController = new CreateClientController();
+
+const createDeliverymanController = new CreateDeliverymanController();
 
 const authenticateClientController = new AuthenticateClientController();
 
 routes.post('/authenticate', authenticateClientController.handle);
 
 routes.post('/client', createClientController.handle);
+
+routes.post('/deliveryman', createDeliverymanController.handle);
 
 export { routes };
