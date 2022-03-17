e8d53093e00d95a3e7523f69a9d4d64ee961547c
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-17 01:07:33 -0300

    chore: added AuthenticateDeliverymanController

---
+++ src/modules/account/authenticateDeliveryman/AuthenticateDeliverymanController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+import { AuthenticateDeliverymanUseCase } from './AuthenticateDeliverymanUseCase';
+
+export class AuthenticateDeliverymanController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const authenticateDeliverymanUseCase = new AuthenticateDeliverymanUseCase();
+
+    const authenticatedDeliveryman =
+      await authenticateDeliverymanUseCase.execute({
+        username,
+        password,
+      });
+
+    return response.json(authenticatedDeliveryman);
+  }
+}
