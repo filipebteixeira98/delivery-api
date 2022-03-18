f2a0a0ce6c56e95fdf3679559241a34ac582a9d3
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-18 01:44:01 -0300

    chore: created middleware ensureAuthenticatedClient

---
+++ src/middlewares/ensureAuthenticatedClient.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticatedClient(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({
+      message: 'Token not provided!',
+    });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub } = verify(
+      token,
+      '7aca59fee3dbb95dab759e2983aeffa9'
+    ) as IPayload;
+
+    request.id_client = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({
+      message: 'Invalid token!',
+    });
+  }
+}
